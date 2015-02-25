7e43651a5b3c55a1d7f25ca60f7f0e9f4871154a
ronald.hsu <hothero0705@gmail.com>
2015-02-25 16:55:31 +0800

    new a dummy ruby file scripts for github language category

---
+++ create_dummy.rb
@@ -0,0 +1,6 @@
+readme_line_count = File.foreach("README.md").inject(0) {|c, line| c+1}
+fp = File.open("dummy.rb", "w")
+
+(1..readme_line_count*2).each { |num| fp.puts("puts \"#{num}\"") }
+
+fp.close
